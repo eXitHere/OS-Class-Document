efd51bd4e34588460f5eb99a8541bfffdcdb23cd
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2021-10-07 14:54:57 +0700

    v3.14159265359

--- CaseStudy#1/benchmark.sh
+++ CaseStudy#1/benchmark.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 time1=$(dotnet run 1)
-time2=$(dotnet run 2)
-time3=$(dotnet run 4)
+echo "1 Thread(s): $time1"
 
-# echo "$time1"
+time2=$(dotnet run 2)
+echo "2 Thread(s): $time2"
+
+time3=$(dotnet run 4)
+echo "4 Thread(s): $time3"
