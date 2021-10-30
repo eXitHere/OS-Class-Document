cf3c952bfd10736774b5180b327d94ad05eb09ea
exitguy <kookza01@gmail.com>
2021-10-30 12:35:55 +0700

    Finished? ex.5

--- Synchronization/example/ex5/Program.cs
+++ Synchronization/example/ex5/Program.cs
@@ -4,48 +4,61 @@ using System.Threading;
 
 namespace OS_Sync_Ex_05
 {
-    class Program
+  class Program
+  {
+    private static string x = "";
+    private static int exitflag = 0;
+    // private static int updateFlag = 0;
+    private static object _Lock = new object();
+
+    static void ThReadX(object i)
     {
-        private static string x = "";
-        private static int exitflag = 0;
-        private static int updateFlag = 0;
-
-        static void ThReadX(object i)
+      while (exitflag == 0)
+      {
+        lock (_Lock)
         {
-            while(exitflag == 0)
-            {
-                if(x != "exit")
-                {
-                    Console.WriteLine("*** Thread {0} : x = {1} ***", i, x);
-                }
-            }
-            Console.WriteLine("--- Thread {0} exit ---", i);
+          Monitor.Wait(_Lock);
+          if (exitflag != 1)
+            Console.WriteLine("*** Thread {0} : x = {1} ***", i, x);
         }
-
-        static void ThWriteX()
-        {
-            string xx;
-            while(exitflag == 0)
-            {
-                Console.Write("input: ");
-                xx = Console.ReadLine();
-                if(xx == "exit")
-                    exitflag = 1;
-                x = xx;
-            }
-        }
-
-        static void Main(string[] args)
-        {
-            Thread A = new Thread(ThWriteX);
-            Thread B = new Thread(ThReadX);
-            Thread C = new Thread(ThReadX);
-            Thread D = new Thread(ThReadX);
-
-            A.Start();
-            B.Start(1);
-            C.Start(2);
-            D.Start(3);
-        }
+      }
+      Console.WriteLine("--- Thread {0} exit ---", i);
     }
+
+    static void ThWriteX()
+    {
+      string xx;
+      while (exitflag == 0)
+      {
+        lock (_Lock)
+        {
+          Monitor.Pulse(_Lock);
+          Console.Write("input: ");
+          xx = Console.ReadLine();
+          if (xx == "exit")
+          {
+            exitflag = 1;
+            Monitor.PulseAll(_Lock); // notify all thread
+          }
+          else
+          {
+            x = xx;
+          }
+        }
+      }
+    }
+
+    static void Main(string[] args)
+    {
+      Thread A = new Thread(ThWriteX);
+      Thread B = new Thread(ThReadX);
+      Thread C = new Thread(ThReadX);
+      Thread D = new Thread(ThReadX);
+
+      A.Start();
+      B.Start(1);
+      C.Start(2);
+      D.Start(3);
+    }
+  }
 }
